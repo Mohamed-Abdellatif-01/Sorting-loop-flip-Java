c5d04651634ce3e383cf34ba7b7a771c25ede5ba
mohamed1714 <44774400+mohamed1714@users.noreply.github.com>
2019-03-01 14:50:58 +0300

    java loops

---
+++ Helloworld/src/com/axcel/exampel/loop.java
@@ -0,0 +1,15 @@
+package com.axcel.exampel;
+
+public class loop {
+
+	public static void main(String[] args) {
+		
+		
+		int[] intarray = new int[] {20,-1,3,55,43,23,10,17,88,2};
+		for (int i=0 ; i < intarray.length; i++)
+			System.out.println("Elemet at index " + i + " : "+ intarray[i]);
+		
+
+	}
+
+}
